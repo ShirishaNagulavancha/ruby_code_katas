557307dfe33c33f22248bf682c417157cd4baf21
shirisha.nagulavanch <shirisha.nagulavancha@careworkstech.com>
2018-04-25 16:54:21 -0400

    block.rb chnages

--- Katas/blocks.rb
+++ Katas/blocks.rb
@@ -7,19 +7,15 @@
 # One method should render all 4 list items and the other should only render 2 list items
 def by_using_call(string1, string2, &block)
   result = ''
-  block_result = ''
   result += wrap_in_tag('h1', string1)
-  block_result = block.call 4
-  result += wrap_in_tag('ul', block_result)
+  result += wrap_in_tag('ul', block.call(4))
   result += wrap_in_tag('h2', string2)
 end
 
 def by_using_yield(string1, string2)
   result = ''
-  yield_result = ''
   result += wrap_in_tag('h1', string1)
-  yield_result = yield 2
-  result += wrap_in_tag('ul', yield_result)
+  result += wrap_in_tag('ul', yield(2))
   result += wrap_in_tag('h2', string2)
 end
 
